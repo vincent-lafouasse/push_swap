2d17ca2e246ce70095741b93fecfb1b7c4db3d3b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 16:10:22 +0200

    sketch deque interface

--- src/stack/t_int_deque.h
+++ src/stack/t_int_deque.h
@@ -6,11 +6,14 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:59:22 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/07/08 16:03:36 by vlafouas         ###   ########.fr       */
+/*   Updated: 2024/07/08 16:10:16 by vlafouas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <stdbool.h>
+#include <stddef.h>
 
 typedef struct s_int_list {
 	int val;
@@ -22,3 +25,13 @@ typedef struct s_int_deque {
 	t_int_list* head;
 	t_int_list* tail;
 } t_int_deque;
+
+t_int_deque deque_new(void);
+int deque_peak_front(t_int_deque dq);
+int deque_peak_back(t_int_deque dq);
+int deque_pop_front(t_int_deque dq);
+int deque_pop_back(t_int_deque dq);
+bool deque_push_front(t_int_deque dq, int val);
+bool deque_push_back(t_int_deque dq, int val);
+bool deque_is_empty(t_int_deque dq);
+size_t deque_size(t_int_deque dq);
