bf7600b5ec1de55fe4c953290249607fcde7892f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 20:59:35 +0200

    format

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,13 +1,15 @@
 #include "gtest/gtest.h"
 
-extern "C" {
+extern "C"
+{
 #include "t_int_deque.h"
 };
 
-TEST(Deque, New) {
-  t_int_deque dq = deque_new();
+TEST(Deque, New)
+{
+    t_int_deque dq = deque_new();
 
-  ASSERT_TRUE(dq.head == nullptr);
-  ASSERT_TRUE(dq.tail == nullptr);
-  FAIL();
+    ASSERT_TRUE(dq.head == nullptr);
+    ASSERT_TRUE(dq.tail == nullptr);
+    FAIL();
 }
