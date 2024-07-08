5b01bcbdc8200357d3c9be41f672ae25c7899575
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-08 20:58:51 +0200

    refactor

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -7,7 +7,7 @@ extern "C" {
 TEST(Deque, New) {
   t_int_deque dq = deque_new();
 
-  ASSERT_TRUE(dq.head == NULL);
-  ASSERT_TRUE(dq.tail == NULL);
+  ASSERT_TRUE(dq.head == nullptr);
+  ASSERT_TRUE(dq.tail == nullptr);
   FAIL();
 }
