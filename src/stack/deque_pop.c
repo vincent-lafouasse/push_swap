5f7087097ea5842680067cf0f8576403c5ff0fa1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:20:28 +0200

    implement pop

--- src/stack/deque_pop.c
+++ src/stack/deque_pop.c
@@ -2,12 +2,38 @@
 
 t_int_list	*deque_pop_front(t_int_deque *dq)
 {
-	(void)dq;
-	return (0);
+	t_int_list* out;
+
+	if (dq->sz == 0)
+		return NULL;
+	if (dq->sz == 1)
+	{
+		out = dq->head;
+		dq->head = dq->tail = NULL;
+		dq->sz = 0;
+		return out;
+	}
+	out = dq->head;
+	dq->head = out->next;
+	dq->head->prev = NULL;
+	return (out);
 }
 
 t_int_list	*deque_pop_back(t_int_deque *dq)
 {
-	(void)dq;
-	return (0);
+	t_int_list* out;
+
+	if (dq->sz == 0)
+		return NULL;
+	if (dq->sz == 1)
+	{
+		out = dq->tail;
+		dq->head = dq->tail = NULL;
+		dq->sz = 0;
+		return out;
+	}
+	out = dq->tail;
+	dq->tail = out->prev;
+	dq->tail->next = NULL;
+	return (out);
 }
