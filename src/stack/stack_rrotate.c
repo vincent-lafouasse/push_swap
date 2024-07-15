12f29d3e94333fc04ecdf890d2b7b9235edcc5d3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:51:33 +0200

    implement rrotate

---
+++ src/stack/stack_rrotate.c
@@ -0,0 +1,21 @@
+#include "t_stack.h"
+
+void	rrotate_a(t_stacks *stacks)
+{
+	if (!stacks)
+		return ;
+	deque_rotate_push(&stacks->a);
+}
+
+void	rrotate_b(t_stacks *stacks)
+{
+	if (!stacks)
+		return ;
+	deque_rotate_push(&stacks->b);
+}
+
+void	rrotate_both(t_stacks *stacks)
+{
+	rrotate_a(stacks);
+	rrotate_b(stacks);
+}
