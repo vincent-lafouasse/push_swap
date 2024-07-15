a9a9159d928176e0a7f7756bc944d06f0eb954e3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:30:01 +0200

    implement rotate

--- src/stack/deque_rotate.c
+++ src/stack/deque_rotate.c
@@ -2,12 +2,28 @@
 
 void	deque_rotate_push(t_int_deque *dq)
 {
+	t_int_list	*node;
+
 	if (!dq || dq->sz == 0 || dq->sz == 1)
 		return ;
+	node = dq->tail;
+	node->prev->next = NULL;
+	node->prev = NULL;
+	node->next = dq->head;
+	dq->head->prev = node;
+	dq->head = node;
 }
 
 void	deque_rotate_pull(t_int_deque *dq)
 {
+	t_int_list	*node;
+
 	if (!dq || dq->sz == 0 || dq->sz == 1)
 		return ;
+	node = dq->head;
+	node->next->prev = NULL;
+	node->next = NULL;
+	node->prev = dq->tail;
+	dq->tail->next = node;
+	dq->tail = node;
 }
