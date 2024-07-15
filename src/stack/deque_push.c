9bdcc2b2dbecfaa1f1b4d051823d780d048bf5d1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:02:10 +0200

    implement push_link

--- src/stack/deque_push.c
+++ src/stack/deque_push.c
@@ -1,5 +1,33 @@
 #include "t_int_deque.h"
 
+void deque_push_front_link(t_int_deque *dq, t_int_list *link)
+{
+	if (!link || !dq)
+		return;
+	if (deque_size(*dq) == 0)
+	{
+		dq->head = dq->tail = link;
+		return;
+	}
+	link->next = dq->head;
+	dq->head->prev = link;
+	dq->head = link;
+}
+
+void deque_push_back_link(t_int_deque *dq, t_int_list *link)
+{
+	if (!link || !dq)
+		return;
+	if (deque_size(*dq) == 0)
+	{
+		dq->head = dq->tail = link;
+		return;
+	}
+	link->prev = dq->tail;
+	dq->tail->next = link;
+	dq->tail = link;
+}
+
 bool	deque_push_front(t_int_deque *dq, int val)
 {
 	(void)dq;
