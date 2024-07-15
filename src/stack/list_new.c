45f1d332744b185d419e23175c972ae004ddc4e7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 14:55:26 +0200

    implement list_new

--- src/stack/list_new.c
+++ src/stack/list_new.c
@@ -1,7 +1,17 @@
 #include "t_int_deque.h"
 
+#include <stdlib.h>
+#include "libft/string.h"
+
 t_int_list	*list_new(int val)
 {
-	(void)val;
-	return (NULL);
+	t_int_list* out;
+
+	out = malloc(sizeof(*out));
+	if (out == NULL)
+		return (NULL);
+	ft_memcpy(out, &val, sizeof(val));
+	out->next = NULL;
+	out->prev = NULL;
+	return out;
 }
