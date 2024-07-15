aa57e14dfe528ee0971be6c5debf6dc07caac576
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 12:58:50 +0200

    implement format script

---
+++ aux/format.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# install the formatter by running `pip3 install c-formatter-42`
+# or `pip3 install --user c-formatter-42` if you don't have root privileges
+
+function norm {
+	echo Formatting "$(basename "$1")"
+	python3 -m c_formatter_42 <"$1" >temp
+	mv temp "$1"
+}
+
+SRC_DIR='./src'
+SRC=$(find $SRC_DIR -name '*.c' -or -name '*.h')
+
+
+for file in $SRC; do
+	norm "$file"
+done
