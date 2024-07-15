0c9c8597ed1466a07bacfb363334963fd01a6636
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 14:57:21 +0200

    impl empty and size

--- src/stack/deque_size.c
+++ src/stack/deque_size.c
@@ -2,12 +2,20 @@
 
 bool	deque_is_empty(const t_int_deque dq)
 {
-	(void)dq;
-	return (true);
+	return (deque_size(dq) == 0);
 }
 
 size_t	deque_size(const t_int_deque dq)
 {
-	(void)dq;
-	return (0);
+	size_t sz;
+	t_int_list* current;
+
+	sz = 0;
+	current = dq.head;
+	while (current)
+	{
+		sz++;
+		current = current->next;
+	}
+	return (sz);
 }
