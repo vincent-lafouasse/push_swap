147b65ef54b3597d51367012f2a4d8ddef2bb539
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:06:23 +0200

    implement clear

--- src/stack/deque_clear.c
+++ src/stack/deque_clear.c
@@ -1,6 +1,17 @@
 #include "t_int_deque.h"
 
+#include <stdlib.h>
+
 void	deque_clear(t_int_deque *dq)
 {
-	(void)dq;
+	t_int_list* current;
+	t_int_list* buffer;
+
+	current = dq->head;
+	while (current)
+	{
+		buffer = current->next;
+		free(current);
+		current = buffer;
+	}
 }
