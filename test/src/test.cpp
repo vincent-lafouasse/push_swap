ca769df6c7265796ac866a17e0c09f809deb12ea
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 21:43:32 +0200

    more tests

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -9,6 +9,7 @@ TEST(Node, New)
 {
     t_int_list* l = list_new(69);
 
+    ASSERT_TRUE(l != nullptr);
     ASSERT_TRUE(l->val == 69);
     ASSERT_TRUE(l->next == nullptr);
     ASSERT_TRUE(l->prev == nullptr);
@@ -21,4 +22,28 @@ TEST(Deque, New)
     ASSERT_TRUE(dq.head == nullptr);
     ASSERT_TRUE(dq.tail == nullptr);
     ASSERT_TRUE(dq.sz == 0);
+}
+
+TEST(Deque, PeekEmpty)
+{
+    t_int_deque dq = deque_new();
+
+    const t_int_list* head = deque_peek_front(dq);
+    const t_int_list* tail = deque_peek_back(dq);
+
+    ASSERT_TRUE(head == nullptr);
+    ASSERT_TRUE(tail == nullptr);
+}
+
+TEST(Deque, PopEmpty)
+{
+    t_int_deque dq = deque_new();
+
+    const t_int_list* head = deque_peek_front(dq);
+    ASSERT_TRUE(head == nullptr);
+
+    dq = deque_new();
+    const t_int_list* tail = deque_peek_back(dq);
+
+    ASSERT_TRUE(tail == nullptr);
 }
