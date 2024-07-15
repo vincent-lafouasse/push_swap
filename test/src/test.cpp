7dba11a07a9d7f160707ce88fc98c8d9ac15eac5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 21:48:49 +0200

    more tests

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -47,3 +47,43 @@ TEST(Deque, PopEmpty)
 
     ASSERT_TRUE(tail == nullptr);
 }
+
+TEST(Deque, PushFrontLink)
+{
+    t_int_deque dq = deque_new();
+
+    deque_push_front_link(&dq, list_new(420));
+    ASSERT_TRUE(dq.head->val == 420);
+    ASSERT_TRUE(dq.tail->val == 420);
+    ASSERT_TRUE(dq.sz == 1);
+
+    deque_push_front_link(&dq, list_new(-1));
+    ASSERT_TRUE(dq.head->val == -1);
+    ASSERT_TRUE(dq.tail->val == 420);
+    ASSERT_TRUE(dq.sz == 2);
+
+    deque_push_front_link(&dq, list_new(69));
+    ASSERT_TRUE(dq.head->val == 69);
+    ASSERT_TRUE(dq.tail->val == 420);
+    ASSERT_TRUE(dq.sz == 3);
+}
+
+TEST(Deque, PushBackLink)
+{
+    t_int_deque dq = deque_new();
+
+    deque_push_back_link(&dq, list_new(0));
+    ASSERT_TRUE(dq.head->val == 0);
+    ASSERT_TRUE(dq.tail->val == 0);
+    ASSERT_TRUE(dq.sz == 1);
+
+    deque_push_back_link(&dq, list_new(420));
+    ASSERT_TRUE(dq.head->val == 0);
+    ASSERT_TRUE(dq.tail->val == 420);
+    ASSERT_TRUE(dq.sz == 2);
+
+    deque_push_back_link(&dq, list_new(69));
+    ASSERT_TRUE(dq.head->val == 0);
+    ASSERT_TRUE(dq.tail->val == 69);
+    ASSERT_TRUE(dq.sz == 3);
+}
