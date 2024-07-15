68b441a8ea40d466c5896c821563bd3a7471735f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 12:19:36 +0200

    refactor

--- src/stack/t_int_deque.h
+++ src/stack/t_int_deque.h
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:59:22 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/07/08 16:12:54 by vlafouas         ###   ########.fr       */
+/*   Updated: 2024/07/15 12:17:10 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-typedef struct s_int_list
+typedef struct s_int_list t_int_list;
+struct s_int_list
 {
-	int					val;
+	const int			val;
 	struct s_int_list	*next;
 	struct s_int_list	*prev;
-}						t_int_list;
+};
 
 typedef struct s_int_deque
 {
