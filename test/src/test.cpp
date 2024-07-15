42781b7487194a5a187e1625bc4d76a67c29b879
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 21:40:22 +0200

    more tests

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -5,11 +5,20 @@ extern "C"
 #include "deque/t_int_deque.h"
 };
 
+TEST(Node, New)
+{
+    t_int_list* l = list_new(69);
+
+    ASSERT_TRUE(l->val == 69);
+    ASSERT_TRUE(l->next == nullptr);
+    ASSERT_TRUE(l->prev == nullptr);
+}
+
 TEST(Deque, New)
 {
     t_int_deque dq = deque_new();
 
     ASSERT_TRUE(dq.head == nullptr);
     ASSERT_TRUE(dq.tail == nullptr);
-    FAIL();
+    ASSERT_TRUE(dq.sz == 0);
 }
