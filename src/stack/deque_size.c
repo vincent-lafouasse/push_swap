02f0a38e1740506cbe1224192d43b4145d5a7791
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:14:08 +0200

    refactor

--- src/stack/deque_size.c
+++ src/stack/deque_size.c
@@ -2,20 +2,10 @@
 
 bool	deque_is_empty(const t_int_deque dq)
 {
-	return (deque_size(dq) == 0);
+	return (dq.sz == 0);
 }
 
 size_t	deque_size(const t_int_deque dq)
 {
-	size_t		sz;
-	t_int_list	*current;
-
-	sz = 0;
-	current = dq.head;
-	while (current)
-	{
-		sz++;
-		current = current->next;
-	}
-	return (sz);
+	return (dq.sz);
 }
