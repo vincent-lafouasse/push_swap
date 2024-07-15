23b84f4da52161933decb8f9e79abce249408cd1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-15 15:37:41 +0200

    lay down interface for stack

--- src/stack/t_stack.h
+++ src/stack/t_stack.h
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:03:19 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/07/15 15:35:20 by poss             ###   ########.fr       */
+/*   Updated: 2024/07/15 15:37:22 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,18 @@ struct					s_stacks
 	t_int_deque			a;
 	t_int_deque			b;
 };
+
+void					swap_a(t_stacks *stacks);
+void					swap_b(t_stacks *stacks);
+void					swap_both(t_stacks *stacks);
+
+void					push_a(t_stacks *stacks);
+void					push_b(t_stacks *stacks);
+
+void					rotate_a(t_stacks *stacks);
+void					rotate_b(t_stacks *stacks);
+void					rotate_both(t_stacks *stacks);
+
+void					rrotate_a(t_stacks *stacks);
+void					rrotate_b(t_stacks *stacks);
+void					rrotate_both(t_stacks *stacks);
