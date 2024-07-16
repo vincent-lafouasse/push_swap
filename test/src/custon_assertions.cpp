f17f04d231077734bc54be6b68dac9db72b0fee3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-16 21:01:03 +0200

    write failing tests

--- test/src/custon_assertions.cpp
+++ test/src/custon_assertions.cpp
@@ -4,17 +4,20 @@
 
 void assert_deque_eq(const t_int_deque dq, const std::vector<int>& expected)
 {
-    ASSERT_TRUE(dq.sz == expected.size()) << "Error, invalid size. expected size " << expected.size() << " was " << dq.sz;
+    ASSERT_TRUE(dq.sz == expected.size())
+        << "Error, invalid size. expected size " << expected.size() << " was "
+        << dq.sz;
 
     t_int_list* current = dq.head;
-    size_t i {};
+    size_t i{};
 
     while (current && i < expected.size())
     {
-        ASSERT_TRUE(current->val == expected[i]) << "Error, expected " << expected[i] <<" was " << current->val;
+        ASSERT_TRUE(current->val == expected[i])
+            << "Error, expected " << expected[i] << " was " << current->val;
         i++;
         current = current->next;
     }
-    //ASSERT_TRUE(current == nullptr);
-    //ASSERT_TRUE(i == expected.size());
+    // ASSERT_TRUE(current == nullptr);
+    // ASSERT_TRUE(i == expected.size());
 }
--- test/src/stack_test.cpp
+++ test/src/stack_test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "gtest/gtest.h"
 
 #include "custom_assertions.h"
@@ -9,12 +10,15 @@ extern "C"
 
 typedef std::vector<int> Vector;
 
-struct Deque {
+struct Deque
+{
     t_int_deque dq;
 
-    static t_int_deque from_vec(const Vector& v) {
+    static t_int_deque from_vec(const Vector& v)
+    {
         t_int_deque dq = deque_new();
-        for (int e: v) {
+        for (int e : v)
+        {
             deque_push_back(&dq, e);
         }
         assert_deque_eq(dq, v);
@@ -22,7 +26,25 @@ struct Deque {
     }
 };
 
-struct Stacks {
+void log_deque(const t_int_deque dq)
+{
+    if (!dq.head)
+        return;
+
+    t_int_list* current = dq.head;
+    std::cout << current->val;
+    current = current->next;
+
+    while (current)
+    {
+        std::cout << "->" << current->val;
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+struct Stacks
+{
     t_stacks s;
 
     static t_stacks from_vecs(const Vector& a, const Vector& b)
@@ -34,15 +56,16 @@ struct Stacks {
     }
 };
 
-TEST(Stack, Swap) {
-    Vector a {1, 2, 3, 4};
-    Vector b {5, 6, 7, 8};
+TEST(Stack, Swap)
+{
+    Vector a{1, 2, 3, 4};
+    Vector b{5, 6, 7, 8};
     t_stacks stacks = Stacks::from_vecs(a, b);
 
-    swap_a(&stacks); 
-    Vector expected_a {2, 1, 3, 4};
-    Vector expected_b {5, 6, 7, 8};
+    swap_a(&stacks);
+    Vector expected_a{2, 1, 3, 4};
+    Vector expected_b{5, 6, 7, 8};
 
-    //assert_deque_eq(stacks.a, expected_a);
-    //assert_deque_eq(stacks.b, expected_b);
+    assert_deque_eq(stacks.a, expected_a);
+    assert_deque_eq(stacks.b, expected_b);
 }
