d60c06e11be2ba6b4f926293b51ad157dc320ef8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 18:25:39 +0200

    sketch checked atoi signature

--- src/load/load.c
+++ src/load/load.c
@@ -1,7 +1,17 @@
 #include "load.h"
 
+#include <stdbool.h>
+
 #include "deque/t_int_deque.h"
 #include "libft/stdlib.h"
+
+typedef struct s_opt_int t_opt_int;
+struct s_opt_int {
+	int val;
+	bool is_valid;
+};
+
+t_opt_int checked_atoi(const char* s);
 
 t_stacks stacks_from_strings(const char** numbers, int len)
 {
