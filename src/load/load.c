f02d4d96bb8db52111c6a6cbe4d3db75921fcf6d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 16:25:20 +0200

    basic loading function

--- src/load/load.c
+++ src/load/load.c
@@ -1,14 +1,20 @@
 #include "load.h"
 
 #include "deque/t_int_deque.h"
+#include "libft/stdlib.h"
 
 t_stacks stacks_from_strings(const char** numbers, int len)
 {
 	t_stacks	stacks;
+	int i;
 
-	(void)numbers;
-	(void)len;
 	stacks.a = deque_new();
 	stacks.b = deque_new();
+	i = 0;
+	while (i < len)
+	{
+		deque_push_back(&stacks.a, ft_atoi(numbers[i]));
+		i++;
+	}
 	return stacks;
 }
