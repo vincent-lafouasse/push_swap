56e680d3f45aa4dc9cf5ec819c48117f2236dffc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 17:41:18 +0200

    implement swap nodes by casting away const (oups)

--- src/deque/deque_sort.c
+++ src/deque/deque_sort.c
@@ -13,14 +13,9 @@ void deque_sort(t_int_deque* dq)
 
 static void swap_with_next(t_int_list* node)
 {
-	t_int_list* next;
-
 	if (!node || !node->next)
 		return;
-	next = node->next;
-	node->prev->next = next;
-	next->prev = node->prev;
-	next->next->prev = node;
+	swap_int((int*)&node->val, (int*)&node->next->val);
 }
 
 static void swap_int(int* a, int* b)
