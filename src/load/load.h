2f88df619bc8cbcada2370027c614f2f91ff7319
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 18:30:16 +0200

    sketch

--- src/load/load.h
+++ src/load/load.h
@@ -2,4 +2,9 @@
 
 #include "stack/t_stacks.h"
 
+/*
+ * load raw stacks = just the numbers, check if valid strings but that's it
+ * load simplified = assign equivalent in N
+*/
+
 t_stacks stacks_from_strings(const char** numbers, int len);
