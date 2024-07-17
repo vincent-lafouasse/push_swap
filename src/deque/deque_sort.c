2fa7b37dd92040660754765180d21693e3c91f79
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 17:45:10 +0200

    sketch

--- src/deque/deque_sort.c
+++ src/deque/deque_sort.c
@@ -7,8 +7,26 @@ static void swap_int(int* a, int* b);
 
 void deque_sort(t_int_deque* dq)
 {
+	bool is_sorted;
+	t_int_list* current;
+
 	if (!dq || dq->sz <= 1)
 		return ;
+	is_sorted = false;
+	current = dq->head;
+	while (!is_sorted)
+	{
+		is_sorted = true;
+		while (current->next)
+		{
+			if (current->val > current->next->val)
+			{
+				swap_with_next(current);
+				is_sorted = false;
+			}
+			current = current->next;
+		}
+	}
 }
 
 static void swap_with_next(t_int_list* node)
