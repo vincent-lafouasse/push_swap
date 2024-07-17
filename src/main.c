3a660494907256a05784baf30b1620f2acfc3992
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-17 16:20:04 +0200

    init empty stacks and load

--- src/main.c
+++ src/main.c
@@ -1,8 +1,12 @@
 #include "stack/t_stacks.h"
+#include "deque/t_int_deque.h"
+#include "log/log.h"
 
 int	main(void)
 {
 	t_stacks	stacks;
 
-	(void)stacks;
+	stacks.a = deque_new();
+	stacks.b = deque_new();
+	log_stacks_horizontal(stacks);
 }
