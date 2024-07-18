5de6cecad9011d30d7ff2f5bc7a06bbe67d6039a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-18 15:20:09 +0200

    sketch

--- src/main.c
+++ src/main.c
@@ -2,6 +2,34 @@
 #include "load/load.h"
 #include "log/log.h"
 
+#include <stdlib.h>
+
+typedef struct s_ordering_ctx {
+	int val;
+	size_t original_ordering;
+} t_ordering_ctx;
+
+t_int_deque simplify(t_int_deque* dq)
+{
+	t_ordering_ctx* original_order;
+	t_int_list* current;
+	size_t i;
+
+	original_order = malloc(dq->sz * sizeof(*original_order));
+	if (!original_order)
+		return deque_new();
+	current = dq->head;
+	i = 0;
+	while (i < dq->sz)
+	{
+		original_order[i] = (t_ordering_ctx){current->val, i};
+		current = current->next;
+		i++;
+	}
+
+	return *dq;
+}
+
 int	main(int ac, char** av)
 {
 	t_stacks	stacks;
