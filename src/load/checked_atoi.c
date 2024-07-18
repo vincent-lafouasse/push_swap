bbe64b5ee86d625b0aa947f853622f0b72f05b88
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-18 18:26:32 +0200

    sketch checked atoi

---
+++ src/load/checked_atoi.c
@@ -0,0 +1,22 @@
+#include "load_internals.h"
+
+#include "libft/ft_string.h"
+
+static t_opt_int invalid(void);
+
+t_opt_int checked_atoi(const char* s)
+{
+	const char* trimmed;
+	int val = 0;
+	if (!s)
+		return invalid();
+	trimmed = ft_strtrim(s, " \v\t\r\n");
+	if (!trimmed)
+		return invalid();
+	return invalid();
+}
+
+static t_opt_int invalid(void)
+{
+	return (t_opt_int){.val = 0, .is_valid = false};
+}
