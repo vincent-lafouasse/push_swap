eab46e970d60174754914a238475569bf51dc5a2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-18 16:15:30 +0200

    bubble sort

--- src/main.c
+++ src/main.c
@@ -65,11 +65,15 @@ void log_array(int* array, size_t len)
 		printf("%d ", array[i]);
 		i++;
 	}
+	printf("\n");
 }
 
 void simplify(t_int_deque* dq)
 {
 	int* data = copy_into_array(*dq);
+	log_array(data, dq->sz);
+	sort_array(data, dq->sz);
+	log_array(data, dq->sz);
 }
 
 int	main(int ac, char** av)
@@ -78,6 +82,5 @@ int	main(int ac, char** av)
 
 	stacks = stacks_from_strings((const char**)av + 1, ac - 1);
 	log_stacks_horizontal(stacks);
-	deque_sort(&stacks.a);
-	log_stacks_horizontal(stacks);
+	simplify(&stacks.a);
 }
