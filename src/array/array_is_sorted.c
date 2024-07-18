4a897eb0f547c5b134b177ae7b03393cb599df08
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-18 17:26:02 +0200

    implement array_is_sorted

---
+++ src/array/array_is_sorted.c
@@ -0,0 +1,14 @@
+#include "array_operations.h"
+
+bool array_is_sorted(const int* array, size_t len)
+{
+	size_t i = 0;
+
+	while (i < len - 1)
+	{
+		if (array[i] > array[i + 1])
+			return false;
+		i++;
+	}
+	return true;
+}
--- src/array/array_operations.h
+++ src/array/array_operations.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdbool.h>
 
 void sort_array(int* array, size_t len);
 int index_of(int value, const int* array, size_t len);
+bool array_is_sorted(const int* array, size_t len);
