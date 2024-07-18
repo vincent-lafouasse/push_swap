2c4746836dd5ca7cfd875d7a42910f7cb6ff7e42
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-07-18 17:30:05 +0200

    refactor

--- src/array/array_operations.h
+++ src/array/array_operations.h
@@ -6,4 +6,4 @@
 void sort_array(int* array, size_t len);
 int index_of(int value, const int* array, size_t len);
 bool array_is_sorted(const int* array, size_t len);
-bool array_contains_duplicates(const int* array, size_t len);
+bool sorted_array_contains_duplicates(const int* sorted_array, size_t len);
--- src/array/contains_duplicates.c
+++ src/array/contains_duplicates.c
@@ -2,14 +2,14 @@
 
 #include <assert.h>
 
-bool array_contains_duplicates(const int* array, size_t len)
+bool array_contains_duplicates(const int* sorted_array, size_t len)
 {
-	assert(array_is_sorted(array, len));
+	assert(array_is_sorted(sorted_array, len));
 	size_t i = 0;
 
 	while (i < len - 1)
 	{
-		if (array[i] == array[i + 1])
+		if (sorted_array[i] == sorted_array[i + 1])
 			return true;
 		i++;
 	}
