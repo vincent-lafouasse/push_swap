7ce34e1b1fe50abd26b8375a365e463c7c204855
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-06 16:20:08 +0200

    implement operation logging

--- src/log/log.c
+++ src/log/log.c
@@ -49,3 +49,35 @@ void log_deque(const t_int_deque dq, const char* name, bool binary)
 	}
 	printf("\n");
 }
+
+void log_operation(t_operation op)
+{
+	if (op == OP_SWAP_A)
+		printf("swap A");
+	else if (op == OP_SWAP_B)
+		printf("swap B");
+	else if (op == OP_SWAP_BOTH)
+		printf("swap both");
+
+	else if (op == OP_PUSH_A)
+		printf("push A");
+	else if (op == OP_PUSH_B)
+		printf("push B");
+
+	else if (op == OP_ROTATE_A)
+		printf("rotate A");
+	else if (op == OP_ROTATE_B)
+		printf("rotate B");
+	else if (op == OP_ROTATE_BOTH)
+		printf("rotate both");
+
+	else if (op == OP_RROTATE_A)
+		printf("rrotate A");
+	else if (op == OP_RROTATE_B)
+		printf("rrotate B");
+	else if (op == OP_RROTATE_BOTH)
+		printf("rrotate both");
+
+	else
+		printf("invalid operation");
+}
--- src/log/log.h
+++ src/log/log.h
@@ -5,3 +5,4 @@
 void log_stacks_horizontal(const t_stacks s, bool binary);
 void log_deque(const t_int_deque dq, const char* name, bool binary);
 void log_int_array(const t_i32* array, size_t len);
+void log_operation(t_operation op);
