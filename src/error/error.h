c4e4d3c3eb899c9e2279fac875ea7b45fbf6c29d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-06 17:12:19 +0200

    introduce t_error type

--- src/error/error.h
+++ src/error/error.h
@@ -2,5 +2,12 @@
 
 #include <stdbool.h>
 
+typedef enum e_error {
+	NO_ERROR,
+	OOM,
+	DUPLICATES,
+	NOT_A_NUMBER,
+} t_error;
+
 void die(const char* msg);
 void assert(bool assertion, const char* msg);
