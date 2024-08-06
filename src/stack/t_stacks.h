7175bc922c71507eec27a9c6eed231e1b2882981
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-06 16:06:43 +0200

    introduce operation enum

--- src/stack/t_stacks.h
+++ src/stack/t_stacks.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   t_stack.h                                          :+:      :+:    :+:   */
+/*   t_stacks.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:03:19 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/07/15 15:37:22 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/06 16:05:45 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,41 @@
 
 #include "deque/t_int_deque.h"
 
-typedef struct s_stacks	t_stacks;
-struct					s_stacks
-{
-	t_int_deque			a;
-	t_int_deque			b;
+typedef struct s_stacks t_stacks;
+struct s_stacks {
+  t_int_deque a;
+  t_int_deque b;
 };
 
-void					swap_a(t_stacks *stacks);
-void					swap_b(t_stacks *stacks);
-void					swap_both(t_stacks *stacks);
+typedef enum e_operations t_operations;
+enum e_operations {
+  OP_SWAP_A,
+  OP_SWAP_B,
+  OP_SWAP_BOTH,
 
-void					push_a(t_stacks *stacks);
-void					push_b(t_stacks *stacks);
+  OP_PUSH_A,
+  OP_PUSH_B,
 
-void					rotate_a(t_stacks *stacks);
-void					rotate_b(t_stacks *stacks);
-void					rotate_both(t_stacks *stacks);
+  OP_ROTATE_A,
+  OP_ROTATE_B,
+  OP_ROTATE_BOTH,
 
-void					rrotate_a(t_stacks *stacks);
-void					rrotate_b(t_stacks *stacks);
-void					rrotate_both(t_stacks *stacks);
+  OP_RROTATE_A,
+  OP_RROTATE_B,
+  OP_RROTATE_BOTH,
+};
+
+void swap_a(t_stacks *stacks);
+void swap_b(t_stacks *stacks);
+void swap_both(t_stacks *stacks);
+
+void push_a(t_stacks *stacks);
+void push_b(t_stacks *stacks);
+
+void rotate_a(t_stacks *stacks);
+void rotate_b(t_stacks *stacks);
+void rotate_both(t_stacks *stacks);
+
+void rrotate_a(t_stacks *stacks);
+void rrotate_b(t_stacks *stacks);
+void rrotate_both(t_stacks *stacks);
