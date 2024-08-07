d34110c4d8fe059c83d92b3e879bc7922a11fc1e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-08 01:43:10 +0200

    refactor

--- aux/input_maker/src/main.rs
+++ aux/input_maker/src/main.rs
@@ -4,15 +4,14 @@ use rand::thread_rng;
 const DEFAULT_SIZE: i32 = 50;
 
 fn main() {
-    let mut rng = thread_rng();
-    let args: Vec<String> = std::env::args().collect();
-
-    let sz: i32 = match args.len() {
-        2 => args[1].parse().unwrap(),
-        _ => DEFAULT_SIZE,
-    };
+    let sz: i32 = std::env::args()
+        .next()
+        .map(|s| s.parse().ok())
+        .flatten()
+        .unwrap_or(DEFAULT_SIZE);
 
     let mut range: Vec<i32> = (-sz..sz).collect();
+    let mut rng = thread_rng();
     range.shuffle(&mut rng);
 
     let sz: usize = sz.try_into().unwrap();
