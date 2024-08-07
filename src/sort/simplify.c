49b0ffa8304f103a81f790beecd49b82e0aa7395
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 18:19:43 +0200

    refactor

--- src/sort/simplify.c
+++ src/sort/simplify.c
@@ -1,30 +1,44 @@
+#include "error/t_error.h"
 #include "sort.h"
 
 #include <stdlib.h>
 #include "array/array_operations.h"
 
-t_error simplify_verify(t_int_deque* dq)
+t_error simplify(t_int_deque* dq, const t_i32* sorted)
 {
-	t_i32* sorted_data = deque_copy_into_array(*dq);
-	if (sorted_data == NULL)
-		return ERROR_OOM;
-	sort_array(sorted_data, dq->sz);
-	if (sorted_array_contains_duplicates(sorted_data, dq->sz))
-		return (free(sorted_data), ERROR_DUPLICATES);
-
-	t_int_deque out = deque_new();
 	int index;
-	t_int_list* current = dq->head;
+	t_int_list* current;
+	t_int_deque out;
+
+	if (!dq || !sorted)
+		return ERROR_NULL_OUT_PARAM;
+	current = dq->head;
+	out = deque_new();
 	while (current)
 	{
-		index = index_of(current->val, sorted_data, dq->sz);
+		index = index_of(current->val, sorted, dq->sz);
 		if (index < 0)
-			return free(sorted_data), ERROR_ELEMENT_NOT_FOUND;
+			return deque_clear(&out), ERROR_ELEMENT_NOT_FOUND;
 		deque_push_back(&out, index);
 		current = current->next;
 	}
 	deque_clear(dq);
 	*dq = out;
-	free(sorted_data);
 	return NO_ERROR;
+}
+
+t_error simplify_verify(t_int_deque* dq)
+{
+	t_i32* sorted_data;
+	t_error err;
+
+	sorted_data= deque_copy_into_array(*dq);
+	if (sorted_data == NULL)
+		return ERROR_OOM;
+	sort_array(sorted_data, dq->sz);
+	if (sorted_array_contains_duplicates(sorted_data, dq->sz))
+		return (free(sorted_data), ERROR_DUPLICATES);
+	err = simplify(dq, sorted_data);
+	free(sorted_data);
+	return err;
 }
