2b042de77662509f8e70dc41fe3c49813e194e90
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 21:56:55 +0200

    basic generator

--- aux/input_maker/src/main.rs
+++ aux/input_maker/src/main.rs
@@ -5,14 +5,23 @@ const DEFAULT_SIZE: i32 = 50;
 
 fn main() {
     let mut rng = thread_rng();
+    let args: Vec<String> = std::env::args().collect();
 
-    let sz: i32 = DEFAULT_SIZE;
+    let sz: i32 = match args.len() {
+        2 => args[1].parse().unwrap(),
+        _ => DEFAULT_SIZE,
+    };
+
     let mut range: Vec<i32> = (-sz..sz).collect();
     range.shuffle(&mut rng);
 
     let sz: usize = sz.try_into().unwrap();
     let slice = &range[..sz];
 
-    let out: String = slice.iter().map(|num| num.to_string()).collect::<Vec<String>>().join(" ");
+    let out: String = slice
+        .iter()
+        .map(|num| num.to_string())
+        .collect::<Vec<String>>()
+        .join(" ");
     println!("{}", out);
 }
