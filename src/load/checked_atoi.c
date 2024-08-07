42c2192a7f82c660f1ae0169f0dc12b682051398
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 13:34:21 +0200

    sketch

--- src/load/checked_atoi.c
+++ src/load/checked_atoi.c
@@ -1,16 +1,35 @@
-#include "libft/ft_string.h"
+#include "error/t_error.h"
 #include "load_internals.h"
+
+static t_opt_uint checked_atou(const char* s);
+
+static t_u32 ft_abs(t_i32 n)
+{
+	if (n >= 0)
+		return n;
+	else
+		return -n;
+}
 
 t_opt_int	checked_atoi(const char *s)
 {
-	const char	*trimmed;
-	t_i32			val;
+	t_u32 absolute_value;
+	t_i8 sign;
 
-	val = 0;
 	if (!s)
 		return (opt_int_none());
-	trimmed = ft_strtrim(s, " \v\t\r\n");
-	if (!trimmed)
-		return (opt_int_none());
-	return (opt_int_none());
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+	else
+		sign = 1;
+	if (checked_atou(s).is_valid)
+		absolute_value = checked_atou(s).val;
+	else
+		return opt_int_none();
+	if ((sign == 1 && absolute_value > INT32_MAX) || (sign == -1 && absolute_value > ft_abs(INT32_MIN)))
+		return opt_int_none();
+	return (t_opt_int){.val=sign * absolute_value, .is_valid=true};
 }
