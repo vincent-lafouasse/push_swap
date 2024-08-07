41f3de1eab979e58b9889f7b9e00d30b89d05ef1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 21:42:35 +0200

    basic shuffle

--- aux/input_maker/src/main.rs
+++ aux/input_maker/src/main.rs
@@ -1,3 +1,12 @@
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+
+const DEFAULT_SIZE: i32 = 1000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = thread_rng();
+    let mut y: Vec<i32> = (1..5).collect();
+    println!("Unshuffled: {:?}", y);
+    y.shuffle(&mut rng);
+    println!("Shuffled:   {:?}", y);
 }
