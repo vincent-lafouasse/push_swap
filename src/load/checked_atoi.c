cfd749b582dee2cedc10883750926c6faf3995a5
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 13:46:59 +0200

    basic atou with no overflow checks

--- src/load/checked_atoi.c
+++ src/load/checked_atoi.c
@@ -30,9 +30,16 @@ t_opt_int	checked_atoi(const char *s)
 
 static t_opt_uint	checked_atou(const char *s)
 {
+	t_u32 val;
 	if (!s || !str_is_all_numbers(s))
 		return opt_uint_none();
-	return opt_uint_none();
+	val = 0;
+	while (*s)
+	{
+		val = 10 * val + (*s - '0');
+		s++;
+	}
+	return ((t_opt_uint){.val = val, .is_valid = true});
 }
 
 static bool str_is_all_numbers(const char* s)
