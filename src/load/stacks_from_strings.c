73cc9d3c1e0cf5a57b158daf4e6fc5b64bbd7bbb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-07 13:21:18 +0200

    refactpr

--- src/load/stacks_from_strings.c
+++ src/load/stacks_from_strings.c
@@ -1,30 +1,28 @@
+#include "deque/t_int_deque.h"
 #include "error/t_error.h"
+#include "libft/stdlib.h" // bad atoi
 #include "load.h"
-
+#include "load_internals.h"
 #include <stdbool.h>
 
-#include "deque/t_int_deque.h"
-#include "load_internals.h"
-#include "libft/stdlib.h" // bad atoi
-
-t_error stacks_from_strings(const char** numbers, int len, t_stacks* out)
+t_error	stacks_from_strings(const char **numbers, int len, t_stacks *out)
 {
 	t_stacks	stacks;
-	int i;
+	int			i;
 
 	if (numbers == NULL || out == NULL)
-		return ERROR_NULL_OUT_PARAM;
+		return (ERROR_NULL_OUT_PARAM);
 	stacks.a = deque_new();
 	stacks.b = deque_new();
 	i = 0;
 	while (i < len)
 	{
 		if (checked_atoi(numbers[i]).is_valid == false)
-			return deque_clear(&stacks.a), ERROR_NOT_A_NUMBER;
+			return (deque_clear(&stacks.a), ERROR_NOT_A_NUMBER);
 		if (deque_push_back(&stacks.a, checked_atoi(numbers[i]).val) == false)
-			return deque_clear(&stacks.a), ERROR_OOM;
+			return (deque_clear(&stacks.a), ERROR_OOM);
 		i++;
 	}
 	*out = stacks;
-	return NO_ERROR;
+	return (NO_ERROR);
 }
