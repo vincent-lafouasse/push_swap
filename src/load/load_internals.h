0a7c557f5c90898bdde260474fd856cdf4fbdb32
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-08 12:26:35 +0200

    restructure

--- src/load/load_internals.h
+++ src/load/load_internals.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "optionals.h"
+#include "stack/t_stacks.h"
+#include "error/t_error.h"
 
+t_error	load_stacks_from_string(const char *numbers, t_stacks *out);
+t_error	load_stacks_from_strings(const char **numbers, int len, t_stacks *out);
 t_opt_int checked_atoi(const char* s);
 t_opt_uint	checked_atou(const char *s);
