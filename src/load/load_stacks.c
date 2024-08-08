36bedaa1dda2254441610a002f030ce6f9a539bc
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-08 12:33:04 +0200

    restructure

--- src/load/load_stacks.c
+++ src/load/load_stacks.c
@@ -1,26 +1,15 @@
-#include "deque/t_int_deque.h"
 #include "error/t_error.h"
 #include "load.h"
 #include "load_internals.h"
 
 t_error	load_stacks(const char **numbers, int len, t_stacks *out)
 {
-	t_stacks	stacks;
-	int			i;
+	t_error err;
 
-	if (numbers == NULL || out == NULL)
-		return (ERROR_NULL_OUT_PARAM);
-	stacks.a = deque_new();
-	stacks.b = deque_new();
-	i = 0;
-	while (i < len)
+	err = load_stacks_from_strings(numbers, len, out);
+	if (err == NO_ERROR)
 	{
-		if (checked_atoi(numbers[i]).is_valid == false)
-			return (deque_clear(&stacks.a), ERROR_NOT_A_NUMBER);
-		if (deque_push_back(&stacks.a, checked_atoi(numbers[i]).val) == false)
-			return (deque_clear(&stacks.a), ERROR_OOM);
-		i++;
+		return NO_ERROR;
 	}
-	*out = stacks;
-	return (NO_ERROR);
+	return (load_stacks_from_string(numbers[0], out));
 }
---
+++ src/load/load_stacks_from_string.c
@@ -0,0 +1,8 @@
+#include "load_internals.h"
+
+t_error load_stacks_from_string(const char* numbers, t_stacks* out)
+{
+	(void)numbers;
+	(void)out;
+	return NO_ERROR;
+}
