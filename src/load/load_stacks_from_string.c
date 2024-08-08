59b70a5857860e0228323a4addd4f71a33b828ad
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-08 12:38:23 +0200

    implement string_array_len

--- src/load/load_stacks_from_string.c
+++ src/load/load_stacks_from_string.c
@@ -1,8 +1,31 @@
 #include "load_internals.h"
+#include "libft/ft_string.h"
+
+static int string_array_len(const char** strs);
 
 t_error load_stacks_from_string(const char* numbers, t_stacks* out)
 {
-	(void)numbers;
-	(void)out;
+	char** split_numbers;
+
+	split_numbers = ft_split(numbers, ' ');
+	if (split_numbers == NULL)
+	{
+		return ERROR_OOM;
+	}
 	return NO_ERROR;
+}
+
+static int string_array_len(const char** strs)
+{
+	int len;
+
+	if (!strs)
+		return 0;
+	len = 0;
+	while (*strs)
+	{
+		strs++;
+		len++;
+	}
+	return len;
 }
