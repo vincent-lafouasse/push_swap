ec6975fad6c787302c4d3d8976de2384e28a04ce
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-13 23:41:45 +0200

    remove forbidden functions

--- src/error/die.c
+++ src/error/die.c
@@ -6,18 +6,21 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 23:31:46 by poss              #+#    #+#             */
-/*   Updated: 2024/08/13 23:31:47 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/13 23:41:25 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "error.h"
-#include <stdio.h> // careful
+#include "libft/ft_io.h"
 #include <stdlib.h>
 
 void	die(const char *msg)
 {
-	printf("Error\n");
+	ft_putstr_fd("Error\n", 2);
 	if (msg)
-		printf("%s\n", msg);
+	{
+		ft_putstr_fd(msg, 2);
+		ft_putstr_fd("\n", 2);
+	}
 	exit(1);
 }
