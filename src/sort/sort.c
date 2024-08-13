821815f68f44f848888f19d1d6227bf68c60dc3f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-14 01:12:23 +0200

    fix variable init

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 23:29:12 by poss              #+#    #+#             */
-/*   Updated: 2024/08/13 23:29:13 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/13 23:56:03 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ t_error	sort(t_stacks *stacks, t_int_deque *ops_out)
 		return (ERROR_NULL_OUT_PARAM);
 	if (stacks->a.sz <= 1)
 	{
-		deque_clear(ops_out);
 		*ops_out = deque_new();
 		return (NO_ERROR);
 	}
