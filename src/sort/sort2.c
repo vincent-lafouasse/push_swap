6d3d1e552daa04e9c457e58857a24b04caad1cbb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-13 21:18:30 +0200

    rename

---
+++ src/sort/sort2.c
@@ -0,0 +1,3 @@
+#include "sort_internals.h"
+
+t_int_deque sort2(t_stacks* stacks);
--- src/sort/sort_internals.h
+++ src/sort/sort_internals.h
@@ -3,5 +3,6 @@
 #include "sort.h"
 
 t_int_deque radix_sort(t_stacks* stacks);
-t_int_deque sort_tiny(t_stacks* stacks);
-t_int_deque sort_small(t_stacks* stacks);
+t_int_deque sort2(t_stacks* stacks);
+t_int_deque sort3(t_stacks* stacks);
+t_int_deque sort5(t_stacks* stacks);
