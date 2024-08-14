d3c96e1f1c53fd759047d5973f7eb7f9085b0285
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-14 23:47:13 +0200

    format

--- aux/test.sh
+++ aux/test.sh
@@ -1,5 +1,11 @@
-ARGS="$(./aux/input_maker/target/debug/input_maker $1)"
-echo "For $1 numbers"
+N_NUMBERS=$1
+
+INPUT_MAKER="aux/input_maker/target/debug/input_maker"
+NUMBERS="$(./${INPUT_MAKER} "${N_NUMBERS}")"
+
+echo "For ${N_NUMBERS} numbers"
+
 echo "Number of operations:"
-./push_swap $ARGS | wc -l
-./push_swap $ARGS | ./aux/checker_Mac $ARGS
+./push_swap $NUMBERS | wc -l
+
+./push_swap $NUMBERS | ./aux/checker_Mac $NUMBERS
