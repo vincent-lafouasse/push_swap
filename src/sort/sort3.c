6333d9389c94553f0fcc1d8145b64e50294ac010
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-14 22:10:16 +0200

    sketch sort3

--- src/sort/sort3.c
+++ src/sort/sort3.c
@@ -6,11 +6,15 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 21:49:07 by poss              #+#    #+#             */
-/*   Updated: 2024/08/14 22:06:10 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/14 22:10:08 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort_internals.h"
+
+#define NEEDS_ROTATE 1
+#define NEEDS_RROTATE -1
+#define ALREADY_SORTED 0
 
 static int try_rotate(t_stacks* stacks)
 {
@@ -18,17 +22,17 @@ static int try_rotate(t_stacks* stacks)
 	if (deque_is_sorted(stacks->a))
 	{
 		rrotate_a(stacks);
-		return 1;
+		return NEEDS_ROTATE;
 	}
 	rrotate_a(stacks);
 	rrotate_a(stacks);
 	if (deque_is_sorted(stacks->a))
 	{
 		rotate_a(stacks);
-		return -1;
+		return NEEDS_RROTATE;
 	}
 	rotate_a(stacks);
-	return 0;
+	return ALREADY_SORTED;
 }
 
 static t_error fill_ops(t_operation op1, t_operation op2, t_int_deque* ops_out)
@@ -37,11 +41,11 @@ static t_error fill_ops(t_operation op1, t_operation op2, t_int_deque* ops_out)
 		return ERROR_NULL_OUT_PARAM;
 	if (op1 == NULL_OP)
 		return NO_ERROR;
-	if (deque_push_front(ops_out, op1) == false)
+	if (deque_push_back(ops_out, op1) == false)
 		return ERROR_OOM;
 	if (op2 == NULL_OP)
 		return NO_ERROR;
-	if (deque_push_front(ops_out, op2) == false)
+	if (deque_push_back(ops_out, op2) == false)
 		return ERROR_OOM;
 	return NO_ERROR;
 }
@@ -51,12 +55,12 @@ t_error	sort3(t_stacks *stacks, t_int_deque *ops_out)
 	*ops_out = deque_new();
 	if (deque_is_sorted(stacks->a))
 		return NO_ERROR;
-	if (try_rotate(stacks) > 0)
+	if (try_rotate(stacks) == NEEDS_ROTATE)
 		return fill_ops(OP_ROTATE_A, NULL_OP, ops_out);
-	if (try_rotate(stacks) < 0)
+	if (try_rotate(stacks) == NEEDS_RROTATE)
 		return fill_ops(OP_RROTATE_A, NULL_OP, ops_out);
 	swap_a(stacks);
-	if (try_rotate(stacks) > 0)
+	if (try_rotate(stacks) == NEEDS_ROTATE)
 		return fill_ops(OP_SWAP_A, OP_ROTATE_A, ops_out);
 	else
 		return fill_ops(OP_SWAP_A, OP_RROTATE_A, ops_out);
