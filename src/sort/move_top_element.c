5f1f2a8b5d4c643cff5c3b0ca24a7f290067c156
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-15 20:34:04 +0200

    implement move top element

--- src/sort/move_top_element.c
+++ src/sort/move_top_element.c
@@ -6,20 +6,22 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 20:21:47 by poss              #+#    #+#             */
-/*   Updated: 2024/08/15 20:28:08 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/15 20:33:41 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sort_internals.h"
 
-t_error	rotate_n_times(size_t n, t_stacks *stacks, t_int_deque *ops_out);
-t_error	reverse_rotate_n_times(size_t n, t_stacks *stacks,
+static t_error	rotate_n_times(size_t n, t_stacks *stacks, t_int_deque *ops_out);
+static t_error	reverse_rotate_n_times(size_t n, t_stacks *stacks,
 			t_int_deque *ops_out);
+
 
 t_error	move_top_element(t_stacks *stacks, t_int_deque *ops_out)
 {
 	int	index_of_min;
 	int	i;
+	t_error err;
 
 	index_of_min = deque_find_min(&stacks->a);
 	i = 0;
@@ -27,18 +29,43 @@ t_error	move_top_element(t_stacks *stacks, t_int_deque *ops_out)
 		return (ERROR_ELEMENT_NOT_FOUND);
 	if (index_of_min < (int)stacks->a.sz - index_of_min)
 	{
-		while (i < index_of_min)
-		{
-			// either rotate or rrotate
-			// and push onto ops
-			i++;
-		}
+		err = rotate_n_times(index_of_min, stacks, ops_out);
+		if (err != NO_ERROR)
+			return err;
 	}
 	else
 	{
-		// opposite
+		err = reverse_rotate_n_times(index_of_min, stacks, ops_out);
+		if (err != NO_ERROR)
+			return err;
 	}
 	push_b(stacks);
-	// signal push b
+	if (deque_push_back(ops_out, OP_PUSH_ONTO_B) == false)
+		return ERROR_OOM;
 	return (NO_ERROR);
+}
+
+static t_error	rotate_n_times(size_t n, t_stacks *stacks, t_int_deque *ops_out)
+{
+	while (n)
+	{
+		rotate_a(stacks);
+		if (deque_push_back(ops_out, OP_ROTATE_A) == false)
+			return ERROR_OOM;
+		n--;
+	}
+	return NO_ERROR;
+}
+
+static t_error	reverse_rotate_n_times(size_t n, t_stacks *stacks,
+			t_int_deque *ops_out)
+{
+	while (n)
+	{
+		rrotate_a(stacks);
+		if (deque_push_back(ops_out, OP_RROTATE_A) == false)
+			return ERROR_OOM;
+		n--;
+	}
+	return NO_ERROR;
 }
