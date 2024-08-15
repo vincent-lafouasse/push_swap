c5bd4e67516dd59bfcc6f1ca6fab6631827650fd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-15 19:49:04 +0200

    refactor

--- src/sort/sort3.c
+++ src/sort/sort3.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 21:49:07 by poss              #+#    #+#             */
-/*   Updated: 2024/08/15 19:45:57 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/15 19:48:43 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,21 +35,7 @@ static int	try_rotate(t_stacks *stacks)
 	return (NOT_SOLVED_BY_ROTATE);
 }
 
-static t_error	fill_ops(t_operation op1, t_operation op2, t_int_deque *ops_out)
-{
-	if (!ops_out)
-		return (ERROR_NULL_OUT_PARAM);
-	*ops_out = deque_new();
-	if (op1 == NULL_OP)
-		return (NO_ERROR);
-	if (deque_push_back(ops_out, op1) == false)
-		return (ERROR_OOM);
-	if (op2 == NULL_OP)
-		return (NO_ERROR);
-	if (deque_push_back(ops_out, op2) == false)
-		return (ERROR_OOM);
-	return (NO_ERROR);
-}
+static t_error	fill_ops(t_operation op1, t_operation op2, t_int_deque *ops_out);
 
 t_error	sort3(t_stacks *stacks, t_int_deque *ops_out)
 {
@@ -68,4 +54,20 @@ t_error	sort3(t_stacks *stacks, t_int_deque *ops_out)
 		return (fill_ops(OP_SWAP_A, OP_ROTATE_A, ops_out));
 	else
 		return (fill_ops(OP_SWAP_A, OP_RROTATE_A, ops_out));
+}
+
+static t_error	fill_ops(t_operation op1, t_operation op2, t_int_deque *ops_out)
+{
+	if (!ops_out)
+		return (ERROR_NULL_OUT_PARAM);
+	*ops_out = deque_new();
+	if (op1 == NULL_OP)
+		return (NO_ERROR);
+	if (deque_push_back(ops_out, op1) == false)
+		return (ERROR_OOM);
+	if (op2 == NULL_OP)
+		return (NO_ERROR);
+	if (deque_push_back(ops_out, op2) == false)
+		return (ERROR_OOM);
+	return (NO_ERROR);
 }
