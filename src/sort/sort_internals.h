275315a7498a628fbd0db4e9459295fb992d5854
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-15 12:58:43 +0200

    init sort5

--- src/sort/sort_internals.h
+++ src/sort/sort_internals.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 23:29:16 by poss              #+#    #+#             */
-/*   Updated: 2024/08/14 21:49:23 by poss             ###   ########.fr       */
+/*   Updated: 2024/08/15 12:58:32 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,3 +18,4 @@
 t_error	radix_sort(t_stacks *stacks, t_int_deque *ops_out);
 t_error	sort2(t_stacks *stacks, t_int_deque *ops_out);
 t_error	sort3(t_stacks *stacks, t_int_deque *ops_out);
+t_error	sort5(t_stacks *stacks, t_int_deque *ops_out);
