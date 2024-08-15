f5b4b9af9c6a6ae30543c0e90aed280e221e0bc3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-15 20:49:10 +0200

    more verbose test script

--- aux/test.sh
+++ aux/test.sh
@@ -3,7 +3,8 @@ N_NUMBERS=$1
 INPUT_MAKER="aux/input_maker/target/debug/input_maker"
 NUMBERS="$(./${INPUT_MAKER} "${N_NUMBERS}")"
 
-echo "For ${N_NUMBERS} numbers"
+echo "For input:"
+echo "$NUMBERS"
 
 echo "Number of operations:"
 ./push_swap $NUMBERS | wc -l
